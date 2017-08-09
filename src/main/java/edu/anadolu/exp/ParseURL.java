40534df0d579893042a81b3d4b19be2ba10d90ae
Ahmet Arslan <iorixxx@yahoo.com>
2017-08-10 01:26:49 +0300

    complement set operation

--- src/main/java/edu/anadolu/exp/ParseURL.java
+++ src/main/java/edu/anadolu/exp/ParseURL.java
@@ -1,8 +1,10 @@
 package edu.anadolu.exp;
 
+import org.apache.lucene.index.PostingsEnum;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.net.URL;
 
 /**
@@ -25,6 +27,108 @@ public class ParseURL {
         System.out.println("ref = " + aURL.getRef());
 
         jSoupTest();
+        System.out.println();
+        int arr1[] = {2, 3, 4, 5, 7, 8, 9, 10};
+        int arr2[] = {2, 3, 6};
+        int m = arr1.length;
+        int n = arr2.length;
+        printDifference(arr1, arr2, m, n);
+
+        int a[] = {1, 2, 3, 4, 5};
+        int b[] = {3, 4, 5, 6, 7, 8};
+
+        printDifference(a, b, a.length, b.length);
+    }
+
+
+    /**
+     * The difference of two posting lists, written A - B is the set of all elements of A that are not elements of B.
+     * <p>
+     * https://www.thoughtco.com/difference-of-two-sets-3126580
+     *
+     * @param first  universal set
+     * @param second negative clause
+     * @return complement of second
+     * @throws IOException if any
+     */
+    private static int differenceOfTwoPostings(PostingsEnum first, PostingsEnum second) throws IOException {
+
+        int count = 0;
+        int firstDocId = first.nextDoc();
+        int secondDocId = second.nextDoc();
+        // We are assuming that docEnum are in doc id order
+        // According to Lucene documentation, the doc ids are in non decreasing order
+        while (true) {
+
+            if (firstDocId == PostingsEnum.NO_MORE_DOCS || secondDocId == PostingsEnum.NO_MORE_DOCS) {
+                break;
+            }
+            if (firstDocId < secondDocId) {
+
+                count++;
+
+                final int freq = first.freq();
+                if (freq != 1) throw new RuntimeException("artificial term frequency should be 1! " + freq);
+                final int docID = first.docID();
+
+                // get document length from this docID
+
+                firstDocId = first.nextDoc();
+
+            } else if (firstDocId > secondDocId) {
+
+                secondDocId = second.nextDoc();
+
+            } else {
+
+                firstDocId = first.nextDoc();
+                secondDocId = second.nextDoc();
+            }
+        }
+
+
+        while (firstDocId != PostingsEnum.NO_MORE_DOCS) {
+
+            count++;
+
+            final int freq = first.freq();
+            if (freq != 1) throw new RuntimeException("artificial term frequency should be 1! " + freq);
+            final int docID = first.docID();
+
+            // get document length from this docID
+
+            firstDocId = first.nextDoc();
+        }
+
+        return count;
+
+    }
+
+
+    /* Function prints all elements of A that are not elements of B. */
+    static void printDifference(int first[], int second[], int m, int n) {
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (first[i] < second[j]) {
+                System.out.println(first[i]);
+                i++;
+            } else if (second[j] < first[i]) {
+                j++;
+            } else {
+                j++;
+                i++;
+            }
+        }
+
+        System.out.println("larger");
+        /* Print remaining elements of the larger array */
+        while (i < m) {
+            System.out.println(first[i] + " ");
+            i++;
+
+        }
+
+        System.out.println("end");
     }
 
 
