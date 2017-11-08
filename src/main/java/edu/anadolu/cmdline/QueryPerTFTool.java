aee5cebfed9c95213b26211539970ddba0af3af2
unknown <gciplak@anadolu.edu.tr>
2017-11-08 21:15:49 +0300

    QueryPerTFTool #4

--- src/main/java/edu/anadolu/cmdline/QueryPerTFTool.java
+++ src/main/java/edu/anadolu/cmdline/QueryPerTFTool.java
@@ -49,7 +49,6 @@ public class QueryPerTFTool extends CmdLineTool {
     private int length = 2;
 
 
-
     @Override
     public void run(Properties props) throws Exception {
 
@@ -71,13 +70,17 @@ public class QueryPerTFTool extends CmdLineTool {
 
         for(InfoNeed need: infoneeds){
             String query = need.query();
-            System.out.println("Query: "+query);
             Analyzer analyzer = Analyzers.analyzer(Tag.tag(tag));
             List<String> tokens = Analyzers.getAnalyzedTokens(query, analyzer);
             if(tokens.size()!=length) continue;
+            System.out.println(query);
+
 
             Set<String> relevantDocs = need.getJudgeMap().entrySet().stream().filter(e -> e.getValue() > 0)
                     .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue())).keySet();
+
+           // Set<String> nonRelevantDocs = need.getJudgeMap().entrySet().stream().filter(e -> e.getValue() <= 0)
+            //        .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue())).keySet();
 
 
             Set<String> fields = new TreeSet<String>();
@@ -92,12 +95,14 @@ public class QueryPerTFTool extends CmdLineTool {
 
 
                 try (IndexReader reader = DirectoryReader.open(FSDirectory.open(path))) {
-                    termFreq4MultipleDocs(tokens,field, reader, relevantDocs);
+                    termFreq4MultipleDocs(tokens, field, reader, relevantDocs);
+                   // System.out.println("NONRELEVANTS");
+                   // termFreq4MultipleDocs(tokens,field, reader, nonRelevantDocs);
                 }
             }
         }
 
-        System.out.println("High Frequency terms extracted in " + execution(start));
+        System.out.println("Term frequencies according to relevant and nonrelevant docs is completed  " + execution(start));
 
     }
 
@@ -126,45 +131,66 @@ public class QueryPerTFTool extends CmdLineTool {
                 if (relevantDocIds.contains(docID)){
                     //Relevant doc is detected
                     ArrayList<TermTFStats> entry = termStats4RelevantDocs.get(docID);
-                    entry.add(new TermTFStats(term.text(),reader.totalTermFreq(term),freq));
+                    entry.add(new TermTFStats(term.text(),reader.totalTermFreq(term),freq,queryTokens.indexOf(token)));
                 }
             }
         }
-        printRsults(termStats4RelevantDocs);
+        printResults(termStats4RelevantDocs);
 
     }
 
-    private static void printRsults(HashMap<String,ArrayList<TermTFStats>> termStats4RelevantDocs) {
-        System.out.print("DocId:\t");
+    private static void printResults(HashMap<String,ArrayList<TermTFStats>> termStats4RelevantDocs) {
+        boolean first2second=true;
+        long TF1=0;
+        long TF2=0;
+        for (Map.Entry<String, ArrayList<TermTFStats>> entry : termStats4RelevantDocs.entrySet()){
+            if(entry.getValue().size()!=2)continue;
+            TF1=entry.getValue().get(0).totalTermFreq();
+            TF2=entry.getValue().get(1).totalTermFreq();
+            if(TF1 > TF2) first2second=false;
+            break;
+        }
+
+        System.out.print("DocId:\t\t");
         for (Map.Entry<String, ArrayList<TermTFStats>> entry : termStats4RelevantDocs.entrySet())
             System.out.printf("%s\t", entry.getKey());
 
-        System.out.print("\nW1 TF:\t");
+        System.out.printf("\nW1 tf:\t%d\t",TF1);
         for (Map.Entry<String, ArrayList<TermTFStats>> entry : termStats4RelevantDocs.entrySet()) {
-            if (entry.getValue().size() > 0)
+            if (entry.getValue().size() > 0 && entry.getValue().get(0).termPositionInQuery() == 0)
                 System.out.printf("%s %d\t", entry.getValue().get(0).term().utf8ToString(), entry.getValue().get(0).termFreq());
             else System.out.printf("%d\t", 0);
         }
-        System.out.print("\nW2 TF:\t");
+        System.out.printf("\nW2 tf:\t%d\t",TF2);
         for (Map.Entry<String, ArrayList<TermTFStats>> entry : termStats4RelevantDocs.entrySet()) {
-            if (entry.getValue().size() > 1)
+            if (entry.getValue().size() > 1 && entry.getValue().get(0).termPositionInQuery() == 1)
                 System.out.printf("%s %d\t", entry.getValue().get(1).term().utf8ToString(), entry.getValue().get(1).termFreq());
             else System.out.printf("%d\t", 0);
         }
 
-        System.out.print("\nRatio:\t");
+        System.out.print("\nRatio:\t\t");
         for (Map.Entry<String, ArrayList<TermTFStats>> entry : termStats4RelevantDocs.entrySet()) {
-            if(entry.getValue().size()==2){
+            if(entry.getValue().size()==0){
+                System.out.println(entry.getKey()+" is skipping...(No term in the document ");
+
+            }
+            else if(entry.getValue().size()==1){
+                if(first2second) {
+                    if (entry.getValue().get(0).termPositionInQuery() == 0) System.out.printf("%s\t", "INFINITY");
+                    else System.out.printf("%.4f\t", 0.0);
+                }else{
+                    if (entry.getValue().get(0).termPositionInQuery() == 0) System.out.printf("%.4f\t", 0.0);
+                    else System.out.printf("%s\t", "INFINITY");
+                }
+            }
+            else if(entry.getValue().size()==2){
                 long w1 = entry.getValue().get(0).termFreq();
                 long w2 = entry.getValue().get(1).termFreq();
                 double ratio;
-                if (w1 > w2) ratio = w2 / (double)w1;
-                else ratio = w1 / (double)w2;
+                if (first2second) ratio = w1 / (double)w2;
+                else ratio = w2 / (double)w1;
                 System.out.printf("%.4f\t", ratio);
-            }else System.out.printf("%.4f\t", 0.0);
-        }
-        for (Map.Entry<String, ArrayList<TermTFStats>> entry : termStats4RelevantDocs.entrySet()) {
-            if (entry.getValue().size() > 2) System.out.println("More than 2 !!! "+entry.getKey());
+            }else System.out.println("More than 2 !!! " + entry.getKey());
         }
         System.out.println("\n=========================================");
     }
@@ -172,12 +198,18 @@ public class QueryPerTFTool extends CmdLineTool {
     private static final class TermTFStats extends TermStatistics {
 
         private final long termFreq;
-        public TermTFStats(String term, long totalTermFreq, long termFreq) {
+        private final int termPositionInQuery;
+        public TermTFStats(String term, long totalTermFreq, long termFreq, int termPositionInQuery) {
             super(new BytesRef(term), 0l, totalTermFreq);
             this.termFreq = termFreq;
+            this.termPositionInQuery = termPositionInQuery;
         }
         public final long termFreq() {
             return termFreq;
+        }
+
+        public final int termPositionInQuery() {
+            return termPositionInQuery;
         }
 
     }
