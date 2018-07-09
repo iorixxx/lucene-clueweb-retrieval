5b1ddbec58583e39bc2693c6711d0879698a89b7
iorixxx <iorixxx@yahoo.com>
2018-07-09 15:02:27 +0300

    remove feature dumper

--- scripts/y.sh
+++ scripts/y.sh
@@ -4,12 +4,14 @@ if [ -z "$TFD_HOME" ]; then
  TFD_HOME=~/TFD_HOME
 fi
 
+mkdir -p "$TFD_HOME"/excels
+touch "$TFD_HOME"/excels/tables.tex
 cat /dev/null > "$TFD_HOME"/excels/tables.tex
 
-for tag in KStemAnalyzerAnchor KStemAnalyzer; do
-for set in CW09A_CW12B MQ09 CW09A CW09B CW12B; do
-#   ./run.sh Y -tag "$tag" -collection "$set" -optimize NDCG20
-    ./run.sh Y -tag "$tag" -collection "$set" -optimize NDCG100
-    ./run.sh Y -tag "$tag" -collection "$set" -optimize MAP
+for tag in KStemAnchor; do
+for set in MQ09 CW09A; do
+#   ./run.sh Y -tag "$tag" -collection "$set" -optimize NDCG20 -var 1 -sigma0
+    ./run.sh Y -tag "$tag" -collection "$set" -optimize NDCG100 -var 1 -sigma0
+    ./run.sh Y -tag "$tag" -collection "$set" -optimize MAP -var 1 -sigma0
 done
 done
