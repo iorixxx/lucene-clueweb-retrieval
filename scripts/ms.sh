bda2c4666f83264d1e01aa3d476f93cc6c8a16d6
iorixxx <iorixxx@yahoo.com>
2018-07-14 21:14:17 +0300

    Model Selection is not thread safe

--- scripts/ms.sh
+++ scripts/ms.sh
@@ -11,8 +11,7 @@ echo 'Model,k,Set,Measure,Accuracy,Anchor'> ms.csv
 for tag in KStemAnchor; do
 for set in MQ09 CW09A; do
     echo "starting MS with set = $set and tag = $tag ..."
-    ./run.sh MS -tag "$tag" -collection "$set" -metric NDCG100  2>> ms.csv &
-    ./run.sh MS -tag "$tag" -collection "$set" -metric NDCG20 2>> ms.csv &
+    ./run.sh MS -tag "$tag" -collection "$set" -metric NDCG100  2>> ms.csv
+    ./run.sh MS -tag "$tag" -collection "$set" -metric NDCG20 2>> ms.csv
 done
-wait
 done
