cb671814c95d4be20d2f469cc9c988cfb62f7e1d
Ahmet Arslan <iorixxx@yahoo.com>
2018-08-11 15:05:58 +0300

    prevent null pointer exception

--- src/main/java/edu/anadolu/exp/TFGraph.java
+++ src/main/java/edu/anadolu/exp/TFGraph.java
@@ -1,9 +1,9 @@
 package edu.anadolu.exp;
 
-import edu.anadolu.datasets.Collection;
-import edu.anadolu.datasets.CollectionFactory;
+import edu.anadolu.analysis.Analyzers;
+import edu.anadolu.analysis.Tag;
 import edu.anadolu.datasets.DataSet;
-import edu.anadolu.similarities.DPH;
+import edu.anadolu.stats.TermStats;
 import org.apache.lucene.search.similarities.ModelBase;
 
 import java.util.List;
@@ -48,13 +48,22 @@ public final class TFGraph extends ScorpioExperiment {
 
         System.out.println();
 
+        String analyzedToken = Analyzers.getAnalyzedToken(t, Analyzers.analyzer(Tag.KStem));
+
+        if (!termStatsMap.containsKey(analyzedToken)) {
+            System.out.println("cannot find " + t + " analyzed " + analyzedToken + " in term statistics map");
+            return;
+        }
+
+        TermStats stats = termStatsMap.get(analyzedToken);
+
         for (int tf = 1; tf <= 200; tf++) {
 
             System.out.print(tf + "\t");
 
             for (ModelBase model : models) {
 
-                double score = model.f(tf, docLength, termStatsMap.get(t).docFreq(), termStatsMap.get(t).totalTermFreq(), numberOfDocuments, numberOfTokens);
+                double score = model.f(tf, docLength, stats.docFreq(), stats.totalTermFreq(), numberOfDocuments, numberOfTokens);
                 System.out.print(String.format("%.7f", score));
                 System.out.print("\t");
 
