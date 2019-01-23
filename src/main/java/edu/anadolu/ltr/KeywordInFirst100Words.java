759c1dfa4be2bec02a0ccbd602ecd9c38d3e6fe8
Ahmet Aydın <ahmetaydin123@gmail.com>
2019-01-23 17:23:26 +0300

    Analyzer errors fixed2

--- src/main/java/edu/anadolu/ltr/KeywordInFirst100Words.java
+++ src/main/java/edu/anadolu/ltr/KeywordInFirst100Words.java
@@ -19,14 +19,12 @@ public class KeywordInFirst100Words implements IDocFeature {
         try {
             String text = base.jDoc.body().text();
             String keyword = MetaTag.enrich3(base.jDoc, "keywords");
-            String[] keywords = keyword.split(",");
             List<String> content = Analyzers.getAnalyzedTokens(text, Analyzers.analyzer(Tag.NoStem));
             String first100words = "";
             for (int i = 0; i < 100; i++) {
                 first100words += content.get(i);
             }
-            for (String token : keywords) {
-                token = Analyzers.getAnalyzedToken(token, Analyzers.analyzer(Tag.KStem));
+            for (String token : Analyzers.getAnalyzedTokens(keyword, Analyzers.analyzer(Tag.NoStem))) {
                 if (first100words.contains(token))
                     return 1;
             }
