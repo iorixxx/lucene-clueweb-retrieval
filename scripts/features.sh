9d7ace1627335f0b9b82403cded2327221e427e2
Ahmet Arslan <iorixxx@yahoo.com>
2021-03-14 18:21:06 +0300

    add script to extract query features for a given dataset

---
+++ scripts/features.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ -z "$TFD_HOME" ]; then
+    TFD_HOME=~/TFD_HOME
+fi
+
+if [ -z "$1" ]; then
+echo "Please enter a collection, which is a required parameter."
+  # Terminate our shell script with success message
+  exit 0
+else
+
+  echo "starting processes with collection = $1 ..."
+  ./run.sh Indexer -collection "$1" -tag NoStem
+  ./run.sh Indexer -collection "$1" -tag KStem
+  ./run.sh Optimize -collection "$1"
+  ./run.sh Doclen -collection "$1"
+  ./run.sh Stats -collection "$1"
+  ./run.sh Stats -collection "$1" -task query
+  ./run.sh TFDistribution -collection "$1" -task query
+  ./run.sh TFDistribution -collection "$1" -task term
+
+fi
